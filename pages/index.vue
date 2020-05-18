f73a3e24576f2806be983047bc56d769cb6c9b72
Oksana Petrusenko <ksupetrusenko@gmail.com>
2020-05-18 13:49:19 +0800

    Project init

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        nuxtjs-starter-pack
+      </h1>
+      <h2 class="subtitle">
+        Nuxt.js project
+      </h2>
+    </div>
+  </section>
+</template>
